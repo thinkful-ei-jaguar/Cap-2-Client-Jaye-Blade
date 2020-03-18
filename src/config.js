e117b82314096332569448d5e60a5b9c9f2b554a
Blade Boles <bladerboles@gmail.com>
2020-03-18 15:31:10 -0600

    Updated endpoint for deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://lit-cliffs-45214.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
+
+//https://lit-cliffs-45214.herokuapp.com/
+
+//'http://localhost:8000/api'
